e881dca46c7db953dae5679ace4df0047a2ffe1a
Ankajrana1 <ankaj@codebrewinnovations.com>
2019-08-29 16:04:29 +0530

    add sign up comopomnet

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() {
+  form : FormGroup;
+
+  name = '';
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+
+    this.form = this.formBuilder.group({
+      first_name: ['', Validators.compose([Validators.required,Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)])],
+      Last_name: ['', Validators.compose([Validators.required,Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)])],
+      Phone_Number: ['', Validators.compose([Validators.minLength(10), Validators.maxLength(10),Validators.required,Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)])],
+    });
     console.log("heloo");
    }
 
